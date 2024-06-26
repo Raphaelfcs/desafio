76e3faa5ab65bed9ccfc34034f7527fdc1f33535
Raphaelfcs <raphaelfcs@gmail.com>
2024-06-25 23:38:21 -0300

    api gtw com token

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,56 +1,38 @@
 resource "aws_api_gateway_rest_api" "api" {
-  name        = "minha-api-gateway"
-  description = "API Gateway pública sem autenticação"
+  name        = "lambda-proxy-api"
+  description = "API Gateway for Lambda Proxy"
 }
 
-resource "aws_api_gateway_resource" "recurso" {
+resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "meu-recurso" # Defina o caminho do seu recurso
+  path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "metodo" {
+resource "aws_api_gateway_method" "proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.recurso.id
-  http_method   = "GET"  # Ou o método HTTP desejado (POST, PUT, etc.)
-  authorization = "NONE" # Desabilita a autenticação
-}
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
 
-
-resource "aws_api_gateway_integration" "integracao_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.recurso.id
-  http_method             = aws_api_gateway_method.metodo.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.minha_lambda.invoke_arn
-}
-
-resource "aws_lambda_permission" "api_gateway_invoke_lambda" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.minha_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
-
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.lambda]
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  # Remova esta linha: stage_name  = "prod"
-
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_integration.lambda))
+  request_parameters = {
+    "method.request.path.proxy" = true
   }
 }
 
-resource "aws_api_gateway_stage" "prod" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  deployment_id = aws_api_gateway_deployment.deployment.id  
-  stage_name    = "prod"
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
 }
 
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [aws_api_gateway_integration.lambda]
 
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+}
 
